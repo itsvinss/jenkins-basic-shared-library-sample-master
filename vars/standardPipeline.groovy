6832a7298ce29f3e5d4c7518e39ed28800eafa1e
Viss1 <vallab.v@gmail.com>
2018-07-22 10:44:07 -0400

    test changes

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -13,21 +13,21 @@ def call(body) {
 	        	checkout scm
 	        }
 	        stage ('Build') {
-	        	sh "echo 'building ${config.projectName} ...'"
+	        	bat "echo 'building ${config.projectName} ...'"
 	        }
 	        stage ('Tests') {
 		        parallel 'static': {
-		            sh "echo 'shell scripts to run static tests...'"
+		            bat "echo 'shell scripts to run static tests...'"
 		        },
 		        'unit': {
-		            sh "echo 'shell scripts to run unit tests...'"
+		            bat "echo 'shell scripts to run unit tests...'"
 		        },
 		        'integration': {
-		            sh "echo 'shell scripts to run integration tests...'"
+		            bat "echo 'shell scripts to run integration tests...'"
 		        }
 	        }
 	      	stage ('Deploy') {
-	            sh "echo 'deploying to server ${config.serverDomain}...'"
+	            bat "echo 'deploying to server ${config.serverDomain}...'"
 	      	}
 	    } catch (err) {
 	        currentBuild.result = 'FAILED'
@@ -35,6 +35,3 @@ def call(body) {
 	    }
     }
 }
-
-
-
